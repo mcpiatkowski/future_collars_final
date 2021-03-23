de98f6147ade15941a08683969199ceb0a5990f1
Michał Piątkowski <mcpiatkowski@gmail.com>
2021-03-23 13:45:09 +0100

    heroku staticfiles_dirs

--- commentsdep/commentsdep/settings.py
+++ commentsdep/commentsdep/settings.py
@@ -135,9 +135,10 @@ STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
+""" 
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
 )
-
+ """
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
