450898c1aba45804e0733651e353cf642171419e
Michał Piątkowski <mcpiatkowski@gmail.com>
2021-03-23 13:50:47 +0100

    heroku media_root

--- commentsdep/commentsdep/settings.py
+++ commentsdep/commentsdep/settings.py
@@ -125,8 +125,9 @@ USE_TZ = True
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #STATIC_URL = '/static/'
 
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
